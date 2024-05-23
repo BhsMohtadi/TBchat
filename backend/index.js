93ad42e17eddce2d670763c9f5d46b0b1e681105
BhsMohtadi <amine.mohtadi10@gmail.com>
2024-05-23 21:13:04 +0100

    backend try 2

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,12 @@ const PORT = process.env.PORT || 9000;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-app.use(cors({
-    origin: 'https://t-bchat-frontend.vercel.app',
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://t-bchat-frontend.vercel.app'); // Replace with your frontend URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 app.use(bodyParser.json());
 
